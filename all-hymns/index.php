166e4717cbb9d6c5646f7044a984c195f657a607
Nathan Vick <nathanvick@Nathans-MacBook-Pro-2.local>
2019-04-07 18:02:51 -0400

    Style and structure changes; add secondary pages

---
+++ all-hymns/index.php
@@ -0,0 +1,43 @@
+<?php include '../components/header.php'; ?>
+
+    <title>Asbury Hymn Project</title>
+</head>
+
+<body id="page-hymns">
+
+    <?php include '../components/navbar.php'; ?>
+
+    <div id="header" class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">All Hymns</h1>
+        </div>
+    </div>
+    <div class="container-fluid pb-4">
+        <div class="row justify-content-center">
+            <div class="col-sm-10">
+                <div class="card">
+                    <h5 class="card-header">Hymns</h5>
+                    <ul class="list-group list-group-flush">
+                        <?php
+                        /*
+                            include '../info.php';
+
+                            $sql = "SELECT name FROM Hymns";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                // output data of each row
+                                while($row = $result->fetch_assoc()) {
+                                    echo "<li class='list-group-item'>" . $row["name"]. "</li>";
+                                }
+                            } else {
+                                echo "0 results";
+                            }
+                            $conn->close(); */
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include '../components/footer.php'; ?>
---
+++ components/navbar.php
@@ -0,0 +1,25 @@
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">
+            <img src="/assets/logo.svg" alt="The Asbury Hymn Project" class="d-none d-sm-block" style="height: 25px;"/>
+            <img src="/assets/logo-mobile.svg" alt="The Asbury Hymn Project" class="d-sm-none" style="height: 75px;"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-content">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item nav-item-home">
+                    <a class="nav-link" href="/"><i class="fas fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item nav-item-about">
+                    <a class="nav-link" href="#"><i class="fas fa-info-circle"></i> About</a>
+                </li>
+                <li class="nav-item nav-item-hymns">
+                    <a class="nav-link" href="/all-hymns"><i class="fas fa-music"></i> All Hymns</a>
+                </li>
+                <li class="nav-item nav-item-classhymns">
+                    <a class="nav-link" href="#"><i class="fas fa-user-friends"></i> Class Hymns</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
