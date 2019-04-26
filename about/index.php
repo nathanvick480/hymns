71e458af7eff3eed972ea51e8bdfb53de34a5222
Nathan Vick <nathan@libraryforafrica.com>
2019-04-26 19:32:06 -0400

    Wording tweaks

--- about/index.php
+++ about/index.php
@@ -18,8 +18,15 @@
                 <div class="card">
                     <div class="card-body">
                         <h3>About Hymns</h3>
+                        <p>Hymns have been a core part of Christian music for centuries. While some may find hymns boring or simplistic, they are actually a highly developed form of music designed for a specific purpose: congregational singing. Hymns use repetition, rhyming, and relatively simple melodic lines to help congregations quickly learn the music and then be able to sing in worship together. Unlike more contemporary forms of music, which often require the use of instruments or worship leaders, hymns are intended to be sung by the congregation members themselves. Hymns are most commonly sung in unison, but they are often written in four-part harmony. They can be sung a cappella or with the accompaniment of organ, piano, or a whole host of instruments. Hymns are an incredibly versatile form of music which has stood the test of time.</p>
 
-                        <p>The following is an alphabetic list of all hymns in the <i>Asbury Hymn Project's</i> collection. Each of these hymns was carefully selected for its beloved status within the Asbury community. Learn more about the selection process on the <a href="/about">About</a> page.</p>
+                        <h3>About the Asbury Hymn Project</h3>
+                        <p>This project was initiated by Nathan Vick and Kellan Kreft after recognizing that awareness of these hymns was declining among their peers, which is especially significant because hymn singing is a major part of Asbury Reunions. After talking with Dr. Schell, Nathan and Kellan also realized that there was no collection or listing of the hymns which have been loved and frequently sung by the Asbury University community throughout the decades. To solve these issues, Nathan and Kellan created the Asbury Hymn Project.</p>
+                        <p>Hymns contained in this website were carefully selected by Nathan and Kellan with the guidance of Dr. Mark Schell, who is the chair of Asbury University's Music Department and has extensive knowledge of Asbury hymnody. Beginning with the list of class hymns, they built a comprehensive list of beloved hymns which represent the heart of Asbury University. Hymns were chosen not because of their popularity at any one point but because of their enduring qualities. While the popularity of some hymns has waxed and waned throughout Asbury's history, every hymn on this website is a hymn which has a special place at Asbury.</p>
+
+                        <h3>Sources</h3>
+                        <p>The majority of material on this website was sourced from interpersonal conversations with Dr. Mark Schell and from public domain resources accessible on <a href="https://hymnary.org" target="_blank">Hymnary.org</a>. Copyrighted material has been noted.</p>
+                        <p>Scripture quotations are from the Revised Standard Version of the Bible, copyright © 1946, 1952, and 1971 National Council of the Churches of Christ in the United States of America. Used by permission. All rights reserved worldwide.</p>
 
                     </div>
                 </div>
